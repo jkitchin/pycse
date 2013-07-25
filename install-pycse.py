0a3d1b55a7fa556da87ebc8df1b19aebd46266dd
John Kitchin <jkitchin@andrew.cmu.edu>
2013-07-25 19:59:56 -0400

    simple script to install pycse. useful on windows to avoid a shell

---
+++ install-pycse.py
@@ -0,0 +1,17 @@
+from setuptools.command import easy_install
+
+
+for package in ['quantities',
+                'uncertainties',
+                'https://github.com/jkitchin/pycse/archive/master.zip',
+                'https://github.com/joblib/pyreport/archive/master.zip']:
+    easy_install.main( ["-U", package] )
+
+## import pip
+
+## for package in ['quantities',
+##                 'uncertainties',
+##                 'https://github.com/jkitchin/pycse/archive/master.zip',
+##                 'https://github.com/joblib/pyreport/archive/master.zip']:
+
+##     pip.main(['install','--upgrade', package]) 
