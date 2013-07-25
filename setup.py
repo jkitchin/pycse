bb32bc02b4ef62a52e9a697aed8afbe8563e1fbc
John Kitchin <jkitchin@cmu.edu>
2013-07-25 09:33:40 -0400

    updated version, took out submit and server scripts from installation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = 'pycse',
-      version='0.1',
+      version='0.2',
       description='python computations in science and engineering',
       url='http://github.com/jkitchin/pycse',
       maintainer='John Kitchin',
@@ -10,6 +10,7 @@ setup(name = 'pycse',
       platforms=['linux'],
       packages=['pycse'],
       scripts=['pycse/bin/publish.py',
-               'pycse/bin/submit.py',
-               'pycse/bin/pycse-server.py'],
+               #'pycse/bin/submit.py',
+               #'pycse/bin/pycse-server.py'
+               ],
       long_description='''python computations in science and engineering''')
