3f35da2702d8e79c96e83cdee358246959a47b8b
John Kitchin <jkitchin@andrew.cmu.edu>
2013-08-03 09:46:12 -0400

    added float comparison operators

---
+++ pycse/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def feq(x, y, epsilon=np.spacing(1)):
+    'x == y'
+    return not((x < (y - epsilon)) or (y < (x - epsilon)))
+
+def flt(x, y, epsilon=np.spacing(1)):
+    'x < y'
+    return x < (y - epsilon)
+
+def fgt(x, y, epsilon=np.spacing(1)):
+    'x > y'
+    return y < (x - epsilon)
+
+def fle(x, y, epsilon=np.spacing(1)):
+    'x <= y'
+    return not(y < (x - epsilon))
+
+def fge(x, y, epsilon=np.spacing(1)):
+    'x >= y'
+    return not(x < (y - epsilon))
