81e077fb95b78d9358d98984d2a4c316967a1dab
John Kitchin <jkitchin@andrew.cmu.edu>
2013-08-26 08:44:53 -0400

    modified setup.py to install uncertainties and quantities packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ print 'Installing ipython magic to : ',IPydir
 
    
 setup(name = 'pycse',
-      version='1.0',
+      version='1.15',
       description='python computations in science and engineering',
       url='http://github.com/jkitchin/pycse',
       maintainer='John Kitchin',
@@ -21,4 +21,21 @@ setup(name = 'pycse',
       packages=['pycse'],
       scripts=['pycse/publish.py'],
       data_files=[(IPydir, ['pycse/00-pycse-magic.py'])],
-      long_description='''python computations in science and engineering''')
+      long_description='''\
+python computations in science and engineering
+===============================================
+
+This package provides some utilities to perform:
+1. linear and nonlinear regression with confidence intervals
+2. Solve some boundary value problems.
+
+See http://jkitchin.github.io/pycse for documentation.
+
+      ''')
+
+import pip
+package = 'https://github.com/jkitchin/pyreport/archive/master.zip'
+pip.main(['install','--upgrade', package])
+pip.main(['install','--upgrade', 'quantities'])
+pip.main(['install','--upgrade', 'uncertainties'])
+# to push to pypi - python setup.py sdist upload
