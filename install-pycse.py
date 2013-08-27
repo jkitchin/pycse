451fe5f3a9083427d73f1cb03786786fccec181c
John Kitchin <jkitchin@andrew.cmu.edu>
2013-08-26 21:16:04 -0400

    Update install-pycse.py
    This is an install script you can run from ipython

--- install-pycse.py
+++ install-pycse.py
@@ -1,16 +1,29 @@
-## from setuptools.command import easy_install
+# for installing magic IPython stuff
 
-## for package in ['quantities',
-##                 'uncertainties',
-##                 'https://github.com/jkitchin/pycse/archive/master.zip',
-##                 'https://github.com/jkitchin/pyreport/archive/master.zip']:
-##     easy_install.main( ["-U", package] )
+from setuptools.command import easy_install
+easy_install.main( ["-U","pycse"] )
+package = 'https://github.com/jkitchin/pyreport/archive/master.zip'
+easy_install.main( ["-U",package] )
 
-import pip
 
-for package in ['quantities',
-                'uncertainties',
-                'https://github.com/jkitchin/pycse/archive/master.zip',
-                'https://github.com/jkitchin/pyreport/archive/master.zip']:
+import IPython, os
+IPydir = os.path.join(IPython.utils.path.get_ipython_dir(),
+                      'profile_default',
+                      'startup')
+                      
+print 'Installing ipython magic to : ',IPydir
 
-    pip.main(['install','--upgrade', package]) 
+if not os.path.exists(IPydir):
+    raise Exception('No ipython directory found')
+
+url = 'https://raw.github.com/jkitchin/pycse/master/pycse/00-pycse-magic.py'
+
+import urllib
+urllib.urlretrieve (url, os.path.join(IPydir,'00-pycse-magic.py'))
+
+print 'Ipython magic installed now!'
+
+# extra packages
+easy_install.main( ["-U","quantities"] )
+easy_install.main( ["-U","uncertainties"] )
+print 'Extra packages now installed.'
