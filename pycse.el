1615951aefd7f8c76cfd95e6e91ed19b397e9559
John Kitchin <jkitchin@cmu.edu>
2014-01-21 16:43:33 -0500

    rebuild pdf after fixing minor typo

--- pycse.el
+++ pycse.el
@@ -13,28 +13,28 @@ c/numpy/reference/generated/numpy.%s.html\">%s</a>" keyword keyword))
      (format "\\href{http://docs.scipy.org/doc/numpy/reference/generated/numpy.%s.html}{%s}"  keyword keyword)))))
 
 
-(require 'org-publish)
-(setq org-publish-project-alist
+(require 'ox-publish)
+(setq org-publish-timestamp-directory "/home-research/jkitchin/Dropbox/books/pycse/"
+ org-publish-project-alist
       '(
 	("pycse-content"
-	 :base-directory "~/Dropbox/pycse/"
+	 :base-directory "~/Dropbox/books/pycse/"
 	 :base-extension "org"
-	 :publishing-directory "~/Dropbox/pycse/gh-pages/"
+	 :publishing-directory "~/Dropbox/books/pycse/gh-pages/"
+	 :publishing-function org-html-publish-to-html
+	 :exclude "gh-pages\\|sandbox\\|pycse\\|octave"
 	 :recursive t
-	 :publishing-function org-publish-org-to-html
 	 :headline-levels 4             ; Just the default for this project.
-	 :auto-preamble t
-	 )
+	 :auto-preamble t)
 	("pycse-static"
-	 :base-directory "~/Dropbox/pycse/"
+	 :base-directory "~/Dropbox/books/pycse/"
 	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|dat\\|mat\\|txt\\|svg"
-	 :publishing-directory "~/Dropbox/pycse/gh-pages/"
-         :exclude "gh-pages"
-	 :recursive t
+	 :publishing-directory "~/Dropbox/books/pycse/gh-pages/"
 	 :publishing-function org-publish-attachment
-	 )
+         :exclude "gh-pages\\|sandbox\\|octave\\|dist"
+	 :recursive t)
 	;; ... all the components ...
-	("pycse" :components ("pycse-content" "pycse-static"))
-      ))
+	("pycse" :components ("pycse-static" "pycse-content"))))
 
-(org-publish "pycse")
+(org-publish "pycse" t)
+
