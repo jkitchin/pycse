97e82c25cc67bcb81185ae46622809bfa4bc69ba
John Kitchin <jkitchin@andrew.cmu.edu>
2014-09-04 08:44:12 -0400

    version bump

--- pycse/__init__.py
+++ pycse/__init__.py
@@ -1,7 +1,8 @@
-__version__ = '1.45'
+__version__ = '1.46'
 '''
 9/16/2013 made odelay multivariable and return arrays
 10/19/2013 changed bvp to work for multiple first order equations.
+9/4/2014 minor changes to odelay documentation and bug fix on endpoint
 '''
 from PYCSE import *
 
