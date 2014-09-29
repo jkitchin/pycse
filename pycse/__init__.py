e31d559b44ee930f05e93dc3e18146b4b73b2e58
John Kitchin <jkitchin@andrew.cmu.edu>
2014-09-29 13:22:34 -0400

    bump version for sandbox

--- pycse/__init__.py
+++ pycse/__init__.py
@@ -1,8 +1,9 @@
-__version__ = '1.47'
+__version__ = '1.48'
 '''
 9/16/2013 made odelay multivariable and return arrays
 10/19/2013 changed bvp to work for multiple first order equations.
 9/4/2014 minor changes to odelay documentation and bug fix on endpoint
+9/29/2014 added sandbox.py
 '''
 from PYCSE import *
 
