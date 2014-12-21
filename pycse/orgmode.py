1fb37626e208d72cc97fae3e1a46e7ec5095ffe0
John Kitchin <jkitchin@andrew.cmu.edu>
2014-12-21 11:16:06 -0500

    added doc

--- pycse/orgmode.py
+++ pycse/orgmode.py
@@ -1,4 +1,9 @@
-'''Module to redirect stderr to stdout for use in Emacs + orgmode'''
+'''Module to redirect stderr to stdout for use in Emacs + orgmode.
+
+org-mode does not capture stderr in code blocks. Here we redirect it so
+that the stderr stream shows up in the results section of a code
+block.
+'''
 
 import sys
 sys.stderr = sys.stdout
