1ebf045deda5c1782aaeb44a5fd01c2e7cf39319
John Kitchin <jkitchin@andrew.cmu.edu>
2014-12-25 21:33:27 -0500

    update doc

--- pycse/utils.py
+++ pycse/utils.py
@@ -24,6 +24,12 @@ def fge(x, y, epsilon=np.spacing(1)):
 from contextlib import contextmanager
 @contextmanager
 def ignore_exception(*exceptions):
+    """Decorator to ignore exceptions.
+
+    >>> with ignore_exception(ZeroDivisionError):
+    ...     print 1/0
+
+    """
     try:
         yield
     except exceptions as e:
