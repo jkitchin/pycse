85de3518cb794c9d6fd522316a5875db97524074
John Kitchin <jkitchin@andrew.cmu.edu>
2015-01-08 12:53:34 -0500

    added test back. fixed the issue!

--- pycse/tests/test_comparisons_units.py
+++ pycse/tests/test_comparisons_units.py
@@ -3,21 +3,21 @@ from pycse.units import *
 from nose.tools import raises
 
 
-# @raises(IncompatibleUnits)
-# def test1():
-#     '''float64 comparison
+@raises(IncompatibleUnits)
+def test1():
+    '''float64 comparison
 
-#     there is a function call order resolution here. when you do
-#     float64 == unit, the __eq__ of the float64 is called, and this
-#     returns true sometimes. it should always return false.
+    there is a function call order resolution here. when you do
+    float64 == unit, the __eq__ of the float64 is called, and this
+    returns true sometimes. it should always return false.
 
-#     unit == float64 works as it should
-#     '''
-#     u = units()
+    unit == float64 works as it should
+    '''
+    u = units()
 
-#     a = np.float64(2)
-#     # should raise an exception
-#     print '{0} float 64 == {1} unit : {2}'.format(a, 2 * u.m, a == 2 * u.m)
+    a = np.float64(2)
+    # should raise an exception
+    print '{0} float 64 == {1} unit : {2}'.format(a, 2 * u.m, a == 2 * u.m)
 
 
 @raises(IncompatibleUnits)
