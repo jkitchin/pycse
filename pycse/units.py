5950415528c15d3d0707da4aeadc353b19de8ac8
John Kitchin <jkitchin@andrew.cmu.edu>
2015-01-08 12:57:00 -0500

    added array_priority to fix eq with float64

--- pycse/units.py
+++ pycse/units.py
@@ -3,16 +3,20 @@
 from __future__ import division
 import numpy as np
 
+
 class IncompatibleUnits(Exception):
     def __init__(self, msg=None):
         self.msg = msg
+
+
     def __str__(self):
         return repr(self.msg)
-    
-class Unit(np.ndarray):
 
+
+class Unit(np.ndarray):
+    __array_priority__ = 2
     BASE_UNITS = ['m', 's', 'kg', 'K', 'mol', 'coul', 'candela']
-    
+
     def __new__(cls, input_array, exponents=None, label=None):
         # Input array is an already formed ndarray instance
         # We first cast to be our class type
@@ -24,17 +28,18 @@ class Unit(np.ndarray):
         return obj
 
     def __array_finalize__(self, obj):
-        if obj is None: return
+        if obj is None:
+            return
         self.exponents = getattr(obj, 'exponents', None)
         self.label = getattr(obj, 'label', None)
-        
+
     def __array_wrap__(self, out_arr, context=None):
         return np.ndarray.__array_wrap__(self, out_arr, context)
-        
+
     def __str__(self):
-        'pretty-print Unit object' 
+        'pretty-print Unit object'
         labelstrings = []
-        for L,e in zip(Unit.BASE_UNITS, self.exponents):
+        for L, e in zip(Unit.BASE_UNITS, self.exponents):
             if e == 0.0:
                 continue
             if np.abs(e % 1) < 1e-6:
@@ -45,10 +50,10 @@ class Unit(np.ndarray):
             else:
                 labelstrings.append('{0}^{1}'.format(L, e))
         LS = '*'.join(labelstrings)
-        
-        known_units = {'m^2*s^-2*kg':'J',
-                       'm*s^-2*kg':'N',
-                       'm^-1*s^-2*kg':'Pa'}
+
+        known_units = {'m^2*s^-2*kg': 'J',
+                       'm*s^-2*kg': 'N',
+                       'm^-1*s^-2*kg': 'Pa'}
         LS = known_units.get(LS, LS)
 
         return '{0} * {1}'.format(np.array(self), LS)
@@ -57,7 +62,7 @@ class Unit(np.ndarray):
         return 'Unit({0}, exponents={1}, label={2})'.format(np.array(self),
                                                             self.exponents,
                                                             self.label)
-                                                            
+
     @property
     def value(self):
         return np.array(self)
@@ -88,22 +93,21 @@ class Unit(np.ndarray):
             f1 = '{{0:{0}}} * {{1}}'.format(fields[0])
             f2 = getattr(u, self.label)
             return f1.format(float(self/f2), self.label)
-            
+
         elif len(fields) == 2:
             f1 = '{{0:{0}}} * {{1}}'.format(fields[0])
-            
+
             f2 = getattr(u, fields[1])
             return f1.format(float(self/f2), self.label)
         else:
             return str(self)
-        
-        
+
 
     @property
     def latex(self):
         'return latex string of units'
         labelstrings = []
-        for L,e in zip(Unit.BASE_UNITS, self.exponents):
+        for L, e in zip(Unit.BASE_UNITS, self.exponents):
             if e == 0.0:
                 continue
             # check for near integer units
@@ -117,29 +121,35 @@ class Unit(np.ndarray):
             else:
                 labelstrings.append('{0}^{1}'.format(L, e))
         return '${0}$'.format(' '.join(labelstrings))
-                
+
     ####################################################################
- 
-    def __lt__(self, other): raise Exception('Not Implemented')
-    def __le__(self, other): raise Exception('Not Implemented')
+
+    def __lt__(self, other):
+        raise Exception('Not Implemented')
+    def __le__(self, other):
+        raise Exception('Not Implemented')
 
     def __eq__(self, other):
         'implement a==b for units'
         if not isinstance(other, Unit):
             raise IncompatibleUnits(['you should not compare objects'
                                      ' with different units.'
-                                     ' {0} == {1}'.format(self,other)])
-        
+                                     ' {0} == {1}'.format(self, other)])
+
         return (np.all(self.exponents == other.exponents)
                 and np.all(np.array(self) == np.array(other)))
 
     def __ne__(self, other):
         return not self == other
-    
-    def __gt__(self, other): raise Exception('Not Implemented')
-    def __ge__(self, other): raise Exception('Not Implemented')   
 
-    ####################################################################  
+    def __gt__(self, other):
+        raise Exception('Not Implemented')
+
+
+    def __ge__(self, other):
+        raise Exception('Not Implemented')
+
+    ####################################################################
 
     def __add__(self, other):
         if not hasattr(other, 'exponents'):
@@ -152,13 +162,13 @@ class Unit(np.ndarray):
 
     def __radd__(self, other):
         return self + other
-    
+
     def __iadd__(self, other):
         self = self + other
         return self
-                
+
     ####################################################################
-    
+
     def __sub__(self, other):
         if not hasattr(other, 'exponents'):
             raise IncompatibleUnits('subtraction, minus non unit')
@@ -167,10 +177,10 @@ class Unit(np.ndarray):
         return Unit(np.array(self) - np.array(other),
                     exponents=self.exponents,
                     label=self.label)
-    
+
     def __rsub__(self, other):
         return self - other
-    
+
     def __isub__(self, other):
         self = self - other
         return self
@@ -182,7 +192,7 @@ class Unit(np.ndarray):
         if isinstance(other, Unit):
             e1 = self.exponents
             e2 = other.exponents
-                     
+
             if np.all((self.exponents + other.exponents) == 0.0):
                 # dimensionless
                 return np.array(self) * np.array(other)
@@ -204,20 +214,20 @@ class Unit(np.ndarray):
             label = '{0}*{1}'.format(label1, label2)
 
             return Unit(np.array(self) * np.array(other),
-                        exponents= e1 + e2,
+                        exponents = e1 + e2,
                         label = label)
- 
+
         else:
             # unit * non-unit
             return Unit(np.array(self) * other,
                     exponents=self.exponents,
                     label=self.label)
-         
-    def __rmul__(self, other):    
+
+    def __rmul__(self, other):
         return Unit(np.array(self) * other,
                     exponents=self.exponents,
                     label=self.label)
-    
+
     def __imul__(self, other):
         self = self * other
         return self
@@ -228,11 +238,11 @@ class Unit(np.ndarray):
         if isinstance(other, Unit):
             e1 = self.exponents
             e2 = other.exponents
-                     
+
             if np.all((self.exponents - other.exponents) == 0.0):
                 # print 'div: result is dimensionless', self, other
                 return np.array(self) / np.array(other)
-           
+
             if ('*' in self. label
                 or '/' in self.label
                 or '^' in self.label):
@@ -259,30 +269,34 @@ class Unit(np.ndarray):
                      label=self.label)
             return U
 
+
     def __rdiv__(self, other):
         # this is for something like other / self
         value = other / np.array(self)
         return Unit(value, -self.exponents,
                     '{0}^-1'.format(self.label))
-        
-    
+
+
     def __idiv__(self, other):
         self = self / other
         return self
 
-    def __floordiv__(self, other): raise Exception('Not Implemented')
-    def __rfloordiv__(self, other): raise Exception('Not Implemented') 
-    def __ifloordiv__(self, other): raise Exception('Not Implemented')    
+    def __floordiv__(self, other):
+        raise Exception('Not Implemented')
+    def __rfloordiv__(self, other):
+        raise Exception('Not Implemented')
+    def __ifloordiv__(self, other):
+        raise Exception('Not Implemented')
 
     __truediv__ = __div__
-    __rtruediv__ = __rdiv__    
-    
+    __rtruediv__ = __rdiv__
+
     def __itruediv__(self, other):
         self = self / other
         return self
-        
+
     #####################################################################
-    
+
     def __pow__(self, other, modulo=None):
         return Unit(np.array(self)**other,
                     self.exponents * other,
@@ -290,7 +304,7 @@ class Unit(np.ndarray):
 
     def __rpow__(self, other):
         raise IncompatibleUnits('You cannot raise a number to a unit!')
-    
+
     def __ipow__(self, other, modulo=None):
         data = np.array(self)
         newexp = self.exponents * other
@@ -300,7 +314,7 @@ class Unit(np.ndarray):
         return self
 
     #####################################################################
-    
+
     def __mod__(self, other): raise Exception('Not Implemented')
     def __imod__(self, other): raise Exception('Not Implemented')
     def __rmod__(self, other): raise Exception('Not Implemented')
@@ -317,19 +331,19 @@ class Unit(np.ndarray):
     def __irshift__(self, other): raise Exception('Not Implemented')
 
     #####################################################################
-    
+
     def __and__(self, other): raise Exception('Not Implemented')
     def __rand__(self, other): raise Exception('Not Implemented')
     def __iand__(self, other): raise Exception('Not Implemented')
 
     #####################################################################
-    
+
     def __xor__(self, other): raise Exception('Not Implemented')
     def __rxor__(self, other): raise Exception('Not Implemented')
     def __ixor__(self, other): raise Exception('Not Implemented')
 
     #####################################################################
-    
+
     def __or__(self, other): raise Exception('Not Implemented')
     def __ror__(self, other): raise Exception('Not Implemented')
     def __ior__(self, other): raise Exception('Not Implemented')
@@ -340,22 +354,22 @@ class Unit(np.ndarray):
         return -1 * self
     def __pos__(self):
         return 1 * self
-    
+
     def __abs__(self):
         return Unit(np.abs(np.array(self)), self.exponents, self.label)
-    
+
     def __invert__(self): raise Exception('Not Implemented')
 
     #####################################################################
-        
+
     def __complex__(self): raise Exception('Not Implemented')
-    
+
     def __int__(self):
         return int(np.asarray(self, np.int))
-                   
+
     def __long__(self):
         return long(np.asarray(self, np.long))
-    
+
     def __float__(self):
         return float(np.asarray(self, np.float))
 
@@ -365,7 +379,7 @@ class Unit(np.ndarray):
     def __hex__(self): raise Exception('Not Implemented')
 
     def __index__(self): raise Exception('Not Implemented')
-    
+
     def __coerce__(self): raise Exception('Not Implemented')
 
     #####################################################################
@@ -453,7 +467,7 @@ def units(base_unit_input=None):
     LENGTH = {}
     LENGTH['km'] = 1000
     LENGTH['m'] = 1
-    LENGTH['dm']=1e-1 
+    LENGTH['dm']=1e-1
     LENGTH['cm'] = 1e-2
     LENGTH['mm'] = 1e-3
     LENGTH['um'] = 1e-6
@@ -480,7 +494,7 @@ def units(base_unit_input=None):
     MASS['ton'] = 2000 * MASS['lb']
     MASS['tonne'] = 1000 * MASS['kg']
     MASS['longton'] = 2240 * MASS['lb']
-    
+
     TIME = {}
     TIME['s'] = 1
     TIME['min'] = 60
@@ -488,33 +502,33 @@ def units(base_unit_input=None):
     TIME['day'] = 24 * TIME['hr']
     TIME['week'] = 7 * TIME['day']
     TIME['year'] = 365.242199 * TIME['day']
-    
+
     TEMPERATURE = {}
     TEMPERATURE['K'] = 1
-    TEMPERATURE['R'] = 5/9 * TEMPERATURE['K'] # I do not understand 
+    TEMPERATURE['R'] = 5/9 * TEMPERATURE['K'] # I do not understand
     # why this is 5/9. I
     # think it should be
     # 9/5, but then no
     # conversions work.
     TEMPERATURE['dC'] = TEMPERATURE['K'] # relative degree C
     TEMPERATURE['dF'] = TEMPERATURE['R'] # relative degree F
-    
+
     MOL = {}
     # you cannot define lbmol or kgmol here because the masses may
     # not be normalized to the chosen base unit above. these units
     # are defined after the base-units are defined and all other
-    # units are normalized. 
+    # units are normalized.
     MOL['mol'] = 1
     MOL['kmol'] = 1000
     MOL['mmol'] = 1e-3
     MOL['molecule'] = 6.022e-23
-    
+
     CHARGE = {}
     CHARGE['coul'] = 1
-    
+
     LUMINOSITY = {}
     LUMINOSITY['cd'] = 1
-    
+
     ALL_UNITS = [LENGTH, TIME, MASS, TEMPERATURE, MOL, CHARGE, LUMINOSITY]
 
     # now we normalize all these defined units by the chosen base
@@ -524,7 +538,7 @@ def units(base_unit_input=None):
     class U:
         'simple storage unit'
         pass
-        
+
 
     UNITS = U()
 
@@ -534,7 +548,7 @@ def units(base_unit_input=None):
         for baseunit in Unit.BASE_UNITS:
             if baseunit in category:
                 NORMV = category[baseunit]
-                
+
                 for key,val in zip(category.keys(),
                                category.values()):
                     e = [0, 0, 0, 0, 0, 0, 0]
@@ -550,37 +564,37 @@ def units(base_unit_input=None):
     UNITS.umol = UNITS.mol / 1e6
 
     #------- Volume -------
-    UNITS.cc = UNITS.cm**3           
-    UNITS.L = 1000 * UNITS.cc           
-    UNITS.mL = UNITS.cc               
-    UNITS.floz = 29.5735297 * UNITS.cc  
-    UNITS.pint = 473.176475 * UNITS.cc  
-    UNITS.quart = 946.35295 * UNITS.cc  
-    UNITS.gal = 3.78541197 * UNITS.L    
+    UNITS.cc = UNITS.cm**3
+    UNITS.L = 1000 * UNITS.cc
+    UNITS.mL = UNITS.cc
+    UNITS.floz = 29.5735297 * UNITS.cc
+    UNITS.pint = 473.176475 * UNITS.cc
+    UNITS.quart = 946.35295 * UNITS.cc
+    UNITS.gal = 3.78541197 * UNITS.L
 
     #---- frequency ----
-    UNITS.Hz = 1 / UNITS.s       
-    UNITS.kHz = 1e3  * UNITS.Hz  
+    UNITS.Hz = 1 / UNITS.s
+    UNITS.kHz = 1e3  * UNITS.Hz
     UNITS.MHz = 1e6  * UNITS.Hz
     UNITS.GHz = 1e9  * UNITS.Hz
 
     #---- force -------
-    UNITS.N = UNITS.kg * UNITS.m / UNITS.s**2   
-    UNITS.dyne = 1e-5 * UNITS.N      
-    UNITS.lbf = 4.44822 * UNITS.N    
+    UNITS.N = UNITS.kg * UNITS.m / UNITS.s**2
+    UNITS.dyne = 1e-5 * UNITS.N
+    UNITS.lbf = 4.44822 * UNITS.N
 
     #----- energy -----
-    UNITS.J = UNITS.kg * UNITS.m**2 / UNITS.s**2 
-    UNITS.MJ = 1e6 * UNITS.J         
-    UNITS.kJ = 1e3 * UNITS.J         
-    UNITS.mJ = 1e-3 * UNITS.J        
-    UNITS.uJ = 1e-6 * UNITS.J        
-    UNITS.nJ = 1e-9 * UNITS.J        
-    UNITS.eV = 1.6022e-19 * UNITS.J    
-    UNITS.BTU = 1.0550559e3 * UNITS.J  
-    UNITS.kWh = 3.6e6 * UNITS.J        
-    UNITS.cal = 4.1868 * UNITS.J       
-    UNITS.kcal = 1e3 * UNITS.cal       
+    UNITS.J = UNITS.kg * UNITS.m**2 / UNITS.s**2
+    UNITS.MJ = 1e6 * UNITS.J
+    UNITS.kJ = 1e3 * UNITS.J
+    UNITS.mJ = 1e-3 * UNITS.J
+    UNITS.uJ = 1e-6 * UNITS.J
+    UNITS.nJ = 1e-9 * UNITS.J
+    UNITS.eV = 1.6022e-19 * UNITS.J
+    UNITS.BTU = 1.0550559e3 * UNITS.J
+    UNITS.kWh = 3.6e6 * UNITS.J
+    UNITS.cal = 4.1868 * UNITS.J
+    UNITS.kcal = 1e3 * UNITS.cal
     UNITS.erg = 1e-7 * UNITS.J
 
     #---- pressure -----
@@ -639,7 +653,7 @@ def units(base_unit_input=None):
     UNITS.farad = UNITS.F
 
     UNITS.X = UNITS.dimensionless = Unit(1, [0, 0, 0, 0, 0, 0, 0], 'dimensionless')
-    
+
     for attr in dir(UNITS):
         if isinstance(getattr(UNITS, attr), Unit):
             u = getattr(UNITS, attr)
