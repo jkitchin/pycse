433a7bb6be5034d616a6c355c7f67595cd56632f
John Kitchin <jkitchin@andrew.cmu.edu>
2015-05-21 12:44:34 -0400

    prettify the code.

--- pycse/utils.py
+++ pycse/utils.py
@@ -1,29 +1,34 @@
 # Copyright 2015, John Kitchin
 # (see accompanying license files for details).
 import numpy as np
+from contextlib import contextmanager
+
 
 def feq(x, y, epsilon=np.spacing(1)):
     """x == y with tolerance"""
     return not((x < (y - epsilon)) or (y < (x - epsilon)))
 
+
 def flt(x, y, epsilon=np.spacing(1)):
     'x < y with tolerance'
     return x < (y - epsilon)
+
 
 def fgt(x, y, epsilon=np.spacing(1)):
     'x > y with tolerance'
     return y < (x - epsilon)
 
+
 def fle(x, y, epsilon=np.spacing(1)):
     'x <= y with tolerance'
     return not(y < (x - epsilon))
+
 
 def fge(x, y, epsilon=np.spacing(1)):
     'x >= y with tolerance'
     return not(x < (y - epsilon))
 
 
-from contextlib import contextmanager
 @contextmanager
 def ignore_exception(*exceptions):
     """Decorator to ignore exceptions.
@@ -35,10 +40,10 @@ def ignore_exception(*exceptions):
     try:
         yield
     except exceptions as e:
-        print 'caught ',e
+        print('caught {}'.format(e))
         return
     finally:
-        print 'done'
+        print('done')
 
 if __name__ == '__main__':
     with ignore_exception(ZeroDivisionError):
