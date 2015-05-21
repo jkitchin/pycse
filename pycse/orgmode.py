86123963cdc54509bd320c02bd52c15f49e006bf
John Kitchin <jkitchin@andrew.cmu.edu>
2015-05-21 12:42:55 -0400

    add copyright header

--- pycse/orgmode.py
+++ pycse/orgmode.py
@@ -3,6 +3,9 @@
 org-mode does not capture stderr in code blocks. Here we redirect it so
 that the stderr stream shows up in the results section of a code
 block.
+
+# Copyright 2015, John Kitchin
+# (see accompanying license files for details).
 '''
 
 import sys
