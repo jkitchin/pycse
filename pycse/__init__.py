8434437911589669119934d0712ad7187fcd3ea2
John Kitchin <jkitchin@andrew.cmu.edu>
2022-04-25 12:42:59 -0400

    flake8 fixes

--- pycse/__init__.py
+++ pycse/__init__.py
@@ -1,24 +1,27 @@
 """Python calculations in Science and Engineering.
 
-Pycse is compatible with Python 3.6.
+Pycse is compatible with Python 3.6+.
 
 """
 
-__version__ = '2.0.0'
+__version__ = "2.0.0"
 
 # * Setup inline images for IPython
 # Make inline figures the default
-try:
-    from IPython import get_ipython
-    from IPython.core.magic import (register_line_magic, register_cell_magic,
-                                register_line_cell_magic)
 
-    from IPython.core.pylabtools import backends
-    import matplotlib as mpl
-    mpl.interactive(True)
-    mpl.use(backends['inline'])
-except:
-    pass
+from IPython import get_ipython
+from IPython.core.magic import (
+    register_line_magic,
+    register_cell_magic,
+    register_line_cell_magic,
+)
+
+from IPython.core.pylabtools import backends
+import matplotlib as mpl
+
+mpl.interactive(True)
+mpl.use(backends["inline"])
+
 
 # * IPython magic for pycse
 # IPython magic functions.
@@ -26,32 +29,38 @@ except:
 # http://ipython.readthedocs.io/en/stable/config/custommagics.html
 
 try:
+
     @register_line_magic
     def pycse_test(line):
-        PASSED = True
+        """Print the versions of libraries installed."""
         import pycse
+
         s = []
-        s += ['pycse version: {0}'.format(pycse.__version__)]
+        s += ["pycse version: {0}".format(pycse.__version__)]
 
         import numpy
-        s += ['numpy version: {0}'.format(numpy.__version__)]
+
+        s += ["numpy version: {0}".format(numpy.__version__)]
 
         import scipy
-        s += ['scipy version: {0}'.format(scipy.__version__)]
+
+        s += ["scipy version: {0}".format(scipy.__version__)]
 
         import matplotlib
-        s += ['matplotlib version: {0}'.format(matplotlib.__version__)]
+
+        s += ["matplotlib version: {0}".format(matplotlib.__version__)]
 
         import IPython
-        s += ['IPython version: {0}'.format(IPython.__version__)]
 
-        return '\n'.join(s)
+        s += ["IPython version: {0}".format(IPython.__version__)]
+
+        return "\n".join(s)
+
 except:
     pass
 
 
 # * load some common libraries
-
 # The goal here is to make it easy for beginners to get started with a minimal
 # amount of importing libraries. This is not what most expert programmers would
 # do, and it is not for them. This tries to mimic the behavior of Matlab where
@@ -60,7 +69,18 @@ except:
 # library imports.
 
 import numpy as np
-from numpy import array, diag, dot, mean, polyfit, polyval, polyder, polyint, std, transpose
+from numpy import (
+    array,
+    diag,
+    dot,
+    mean,
+    polyfit,
+    polyval,
+    polyder,
+    polyint,
+    std,
+    transpose,
+)
 import numpy.linalg as la
 from numpy.linalg import det, eig, eigvals, inv, solve, svd
 
@@ -71,14 +91,14 @@ from scipy.integrate import quad, solve_ivp, solve_bvp
 from scipy.optimize import fsolve, root
 from scipy.interpolate import interp1d
 
-from .PYCSE import polyfit, regress, nlinfit, Rsquared
+from .PYCSE import regress, nlinfit, Rsquared
 from .utils import feq, flt, fgt, fle, fge, read_gsheet
 
 from .beginner import *
 
 # We try this because it fails if you are not in colab, e.g. in a regular
-# Jupyter notebook.
+# Jupyter notebook. ModuleNotFoundError: No module named 'google.colab'
 try:
     from .colab import *
-except:
+except ModuleNotFoundError:
     pass
